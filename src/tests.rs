3e8cd15f9ca0fe5e90611690d60013b6ce176351
Vladyslav-Bochok <vladbochok1@gmail.com>
2021-05-13 15:59:58 +0300

    Initial commit

---
+++ src/tests.rs
@@ -0,0 +1,105 @@
+use crate::{
+    struct_builder::StructBuilder,
+    typed_structure::{EIP712TypedStructure, Eip712Domain},
+};
+use std::str::FromStr;
+use web3::types::{Address, H256, U256};
+
+#[derive(Clone)]
+struct Person {
+    name: String,
+    wallet: Address,
+}
+
+impl EIP712TypedStructure for Person {
+    const TYPE_NAME: &'static str = "Person";
+
+    fn build_structure<BUILDER: StructBuilder>(&self, builder: &mut BUILDER) {
+        builder.add_member("name", &self.name);
+        builder.add_member("wallet", &self.wallet);
+    }
+}
+
+#[derive(Clone)]
+struct Mail {
+    from: Person,
+    to: Person,
+    contents: String,
+}
+
+impl EIP712TypedStructure for Mail {
+    const TYPE_NAME: &'static str = "Mail";
+    fn build_structure<BUILDER: StructBuilder>(&self, builder: &mut BUILDER) {
+        builder.add_member("from", &self.from);
+        builder.add_member("to", &self.to);
+        builder.add_member("contents", &self.contents);
+    }
+}
+
+#[test]
+fn test_encode_eip712_typed_struct() {
+    let domain = Eip712Domain {
+        name: "Ether Mail".to_owned(),
+        version: "1".to_owned(),
+        chain_id: U256::from(1),
+        verifying_contract: Address::from_str("CcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC").unwrap(),
+    };
+
+    let message = Mail {
+        from: Person {
+            name: "Cow".to_owned(),
+            wallet: Address::from_str("CD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826").unwrap(),
+        },
+        to: Person {
+            name: "Bob".to_owned(),
+            wallet: Address::from_str("bBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB").unwrap(),
+        },
+        contents: "Hello, Bob!".to_string(),
+    };
+
+    assert_eq!(
+        &message.encode_type(),
+        "Mail(Person from,Person to,string contents)Person(string name,address wallet)"
+    );
+
+    assert_eq!(
+        &message.encode_data()[..],
+        [
+            H256::from_str("fc71e5fa27ff56c350aa531bc129ebdf613b772b6604664f5d8dbe21b85eb0c8")
+                .unwrap(),
+            H256::from_str("cd54f074a4af31b4411ff6a60c9719dbd559c221c8ac3492d9d872b041d703d1")
+                .unwrap(),
+            H256::from_str("b5aadf3154a261abdd9086fc627b61efca26ae5702701d05cd2305f7c52a2fc8")
+                .unwrap()
+        ]
+    );
+
+    assert_eq!(
+        message.hash_struct(),
+        H256::from_str("c52c0ee5d84264471806290a3f2c4cecfc5490626bf912d01f240d7a274b371e").unwrap()
+    );
+
+    assert_eq!(
+        &domain.encode_type(),
+        "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+    );
+
+    assert_eq!(
+        &domain.encode_data()[..],
+        [
+            H256::from_str("c70ef06638535b4881fafcac8287e210e3769ff1a8e91f1b95d6246e61e4d3c6")
+                .unwrap(),
+            H256::from_str("c89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6")
+                .unwrap(),
+            H256::from_str("0000000000000000000000000000000000000000000000000000000000000001")
+                .unwrap(),
+            H256::from_str("000000000000000000000000CcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC")
+                .unwrap(),
+        ]
+    );
+
+    assert_eq!(
+        domain.hash_struct(),
+        H256::from_str("f2cee375fa42b42143804025fc449deafd50cc031ca257e0b194a650a912090f").unwrap()
+    );
+}
---
+++ src/typed_structure.rs
@@ -0,0 +1,159 @@
+use parity_crypto::Keccak256;
+use web3::types::{Address, H256, U256};
+
+use crate::struct_builder::{
+    EncodeBuilder, StructBuilder, TypeBuilder,
+};
+
+#[derive(Debug, Clone)]
+pub struct EncodedStructureMember {
+    /// Type identifier.
+    pub member_type: String,
+    /// Name identifier.
+    pub name: String,
+    /// Flag denoting structure or elementary type.
+    pub is_reference_type: bool,
+    // Members that are inside this member.
+    pub inner_members: Vec<EncodedStructureMember>,
+}
+
+impl EncodedStructureMember {
+    pub fn encode<MEMBER: StructMember>(name: &str, member: &MEMBER) -> Self {
+        Self {
+            member_type: member.member_type(),
+            name: name.to_string(),
+            is_reference_type: member.is_reference_type(),
+            inner_members: member.get_inner_members(),
+        }
+    }
+
+    /// Encodes the structure as `name ‖ "(" ‖ member₁ ‖ "," ‖ member₂ ‖ "," ‖ … ‖ memberₙ ")".
+    pub fn get_encoded_type(&self) -> String {
+        let mut encoded_type = String::new();
+        encoded_type.push_str(&self.member_type);
+        encoded_type.push('(');
+
+        let mut members = self.inner_members.iter();
+
+        if let Some(member) = members.next() {
+            encoded_type.push_str(&member.member_type);
+            encoded_type.push(' ');
+            encoded_type.push_str(&member.name);
+        }
+        for member in members {
+            encoded_type.push(',');
+            encoded_type.push_str(&member.member_type);
+            encoded_type.push(' ');
+            encoded_type.push_str(&member.name);
+        }
+
+        encoded_type.push(')');
+
+        encoded_type
+    }
+}
+
+pub trait StructMember {
+    const MEMBER_TYPE: &'static str;
+    const IS_REFERENCE_TYPE: bool;
+
+    fn member_type(&self) -> String {
+        Self::MEMBER_TYPE.to_string()
+    }
+
+    fn is_reference_type(&self) -> bool {
+        Self::IS_REFERENCE_TYPE
+    }
+
+    fn get_inner_members(&self) -> Vec<EncodedStructureMember>;
+
+    fn encode_member_data(&self) -> H256;
+}
+
+impl<TypedStructure: EIP712TypedStructure> StructMember for TypedStructure {
+    const MEMBER_TYPE: &'static str = Self::TYPE_NAME;
+    const IS_REFERENCE_TYPE: bool = true;
+
+    fn get_inner_members(&self) -> Vec<EncodedStructureMember> {
+        let mut builder = TypeBuilder::new();
+        self.build_structure(&mut builder);
+
+        builder.get_inner_members()
+    }
+
+    fn encode_member_data(&self) -> H256 {
+        self.hash_struct()
+    }
+}
+
+/// Interface for defining the structure for the EIP712 signature.
+pub trait EIP712TypedStructure {
+    const TYPE_NAME: &'static str;
+
+    fn build_structure<BUILDER: StructBuilder>(&self, builder: &mut BUILDER);
+
+    fn encode_type(&self) -> String {
+        let mut builder = EncodeBuilder::new();
+        self.build_structure(&mut builder);
+
+        builder.encode_type(&Self::TYPE_NAME)
+    }
+
+    fn encode_data(&self) -> Vec<H256> {
+        let mut builder = EncodeBuilder::new();
+        self.build_structure(&mut builder);
+
+        builder.encode_data()
+    }
+
+    fn hash_struct(&self) -> H256 {
+        // hashStruct(s : 𝕊) = keccak256(keccak256(encodeType(typeOf(s))) ‖ encodeData(s)).
+        let type_hash = {
+            let encode_type = self.encode_type();
+            encode_type.keccak256()
+        };
+        let encode_data = self.encode_data();
+
+        let mut bytes = Vec::new();
+        bytes.extend_from_slice(&type_hash);
+        for data in encode_data {
+            bytes.extend_from_slice(&data.as_bytes());
+        }
+
+        bytes.keccak256().into()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Eip712Domain {
+    /// The user readable name of signing domain, i.e. the name of the DApp or the protocol.
+    pub name: String,
+    /// The current major version of the signing domain. Signatures from different versions are not compatible.
+    pub version: String,
+    /// The [EIP-155](https://eips.ethereum.org/EIPS/eip-155) chain id.
+    pub chain_id: U256,
+    /// the address of the contract that will verify the signature.
+    pub verifying_contract: Address,
+}
+
+impl Eip712Domain {
+    pub fn new(name: String, version: String, verifying_contract: Address, chain_id: U256) -> Self {
+        Self {
+            name,
+            version,
+            verifying_contract,
+            chain_id: chain_id,
+        }
+    }
+}
+
+impl EIP712TypedStructure for Eip712Domain {
+    const TYPE_NAME: &'static str = "EIP712Domain";
+
+    fn build_structure<BUILDER: StructBuilder>(&self, builder: &mut BUILDER) {
+        builder.add_member("name", &self.name);
+        builder.add_member("version", &self.version);
+        builder.add_member("chainId", &self.chain_id);
+        builder.add_member("verifyingContract", &self.verifying_contract);
+    }
+}
